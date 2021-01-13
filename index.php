0d0e7717db93e229c58b147826c68988e8c3d80e
zZeTraL <lucashbs.pro@gmail.com>
2021-01-14 00:12:43 +0100

    Animation and responsive update

--- index.php
+++ index.php
@@ -3,22 +3,24 @@
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        <meta name="description" content="Cas'ISEN est un site de partage des meilleurs plans et bonus du moment mais pas que... Retrouvez également toute l'actualité mondiale du casino en ligne !">
+        
         <!-- CSS -->
         <link rel="stylesheet" href="../CasISEN/css/body.css">
         <link rel="stylesheet" href="../CasISEN/css/header.css">
         <link rel="stylesheet" href="../CasISEN/css/hover.css">
         <link rel="stylesheet" href="../CasISEN/css/main.css">
 
-        <!-- FONTAWESOME ONLINE -->
+        <!-- FONTAWESOME LIB -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- AOS LIB -->
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
+        <!-- BOOTSTRAP -->
         <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">-->
 
-        <title>CasISEN</title>
+        <title>Cas'ISEN | Bienvenue</title>
 </head>
 <body>
 
@@ -29,7 +31,7 @@
     </header>
 
     <main class="aos-animate">
-        <article style="height: 2000px;">
+        <article class="h-500">
             <div class="container-section aos-animate" data-aos="fade">
                 <h1>Bienvenue sur Cas'ISEN !</h1>
                 <h3>
@@ -39,31 +41,58 @@
             </div>
             <section class="section ptsforts-background">
                 <div class="container-section aos-animate" data-aos="zoom-in">
-                    <h2>Nos points forts</h2>
+                    <h1>Nos points forts</h1>
                     <div class="ptsforts-col">
                         <div class="ptsforts-col-element aos-animate" data-aos="zoom-in">
+                            <img src="../CasISEN/img/ponctualite.svg" alt="Logo" class="col-img">
+                            <h4>Ponctualité</h4>
+                            <p>Retrouvez chaque jour des nouvelles de l'actualité autour des casinos, ne loupez aucune information !</p>
+                        </div>
+                        <div class="ptsforts-col-element aos-animate" data-aos="zoom-in">
+                            <img src="../CasISEN/img/fiabilite.svg" alt="Logo" class="col-img">
+                            <h4>Fiabilité</h4>
+                            <p>Tous nos articles et avis sont certifiés et validés par des professionnels !</p>
+                        </div>
+                        <div class="ptsforts-col-element aos-animate" data-aos="zoom-in">
                             <img src="../CasISEN/img/clock.svg" alt="Logo" class="col-img">
-                            <h4>Ponctualité</h4>
-                            <p>Retrouvez chaque jour des nouvelles de l'actualité mondiale autour des casinos, ne louper aucune information !</p>
-                        </div>
-                        <div class="ptsforts-col-element aos-animate" data-aos="zoom-in">
-                            <img src="../CasISEN/img/logo.png" alt="Logo" class="nav-logo">
-                            <h4>Ponctualité</h4>
-                        </div>
-                        <div class="ptsforts-col-element aos-animate" data-aos="zoom-in">
-                            <img src="../CasISEN/img/logo.png" alt="Logo" class="nav-logo">
-                            <h4>Ponctualité</h4>
+                            <h4>JE SAIS PAS</h4>
+                            <p>Retrouvez chaque jour des nouvelles de l'actualité mondiale autour des casinos, ne loupez aucune information !</p>
                         </div>
                     </div>
                 </div>
             </section>
             <section class="section">
-                <div class="container-section aos-animate" data-aos="zoom-in">
+                <div class="container-section aos-animate" data-aos="fade">
                     <h1>En ce moment</h1>
                     <h3>
                         Retrouvez une sélection d'article d'actualité et de bonus inédits !
                     </h3>
-                    <div class="figure-background">
+                    <div class="figure-background" data-aos="flip-right" data-aos-delay="250" data-aos-duration="1000">
+                        <div class="col-4">
+                            <img src="../CasISEN/img/gamdom.png" alt="Logo" class="col-4-img">
+                            <div class="casino-url">
+                                <a href="https://www.gamdom.com/" class="">
+                                    <div>
+                                        <span class="url-title">Gamdom</span>
+                                    </div>
+                                    <span class="url-desc">gandom.com</span>
+                                </a>
+                            </div>
+                            <svg class="svg-green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 89 70" id="gamdomarrow"><g fill="none" fill-rule="evenodd"><g mask="url(#ayb)"><path fill="#0EE07B" d="M26.253 37L26 44l-62 63h28.458L54 44v-7z"></path><path fill="#0EE07B" d="M26 40h27.646v-4.315l.354-.36V17.349c0-3.595-1.098-6.636-3.225-9.153L26 33.169V40z"></path><path fill="#0EE07B" d="M131.305-40.452L141-30.516V-66h-35.547l10.054 9.935-64.732 64.26-16.851 17.021L54.107 33"></path><path fill="#0A7843" d="M26 44h28V33H26z"></path></g></g></svg>
+                            <div class="figure-desc">
+                                <p>Un coffre <strong>GRATUIT</strong><br>et jusqu'à 50$ avec le code 
+                                <strong>"CASISEN"</strong></p>
+                            </div>
+                            <div class="button-resp">
+                                <button type="button" class="button-bonus">
+                                    <a href="">
+                                        Récupère <b>ton bonus</b>
+                                    </a>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="figure-background" data-aos="flip-right" data-aos-delay="450" data-aos-duration="1000">
                         <div class="col-4">
                             <img src="../CasISEN/img/gamdom.png" alt="Logo" class="col-4-img">
                             <div class="casino-url">
@@ -94,6 +123,10 @@
 
         </article>
     </main>
+
+    <footer class="aos-animate">
+
+    </footer>
 
     <!--<script src="../CasISEN/js/scroll.js"></script>-->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
--- news.php
+++ news.php
@@ -3,54 +3,34 @@
 <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+        <meta name="description" content="Cas'ISEN est un site de partage des meilleurs plans et bonus du moment mais pas que... Retrouvez également toute l'actualité mondiale du casino en ligne !">
+        
         <!-- CSS -->
         <link rel="stylesheet" href="../CasISEN/css/body.css">
         <link rel="stylesheet" href="../CasISEN/css/header.css">
         <link rel="stylesheet" href="../CasISEN/css/hover.css">
         <link rel="stylesheet" href="../CasISEN/css/main.css">
 
-        <!-- FONTAWESOME ONLINE -->
+        <!-- FONTAWESOME LIB -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- AOS LIB -->
         <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+        <!-- BOOTSTRAP -->
+        <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">-->
 
-        <title>CasISEN</title>
+        <title>Cas'ISEN | Actualités</title>
 </head>
 <body>
 
+
+    <header class="header-container header-img gradient">
+        <?php require "../CasISEN/parts/nav.php"; ?>
+        <div class="icon-scroll"></div>
+    </header>
+
     <main class="aos-animate">
-    <section class="section">
-                <div class="container-section aos-animate" data-aos="zoom-in">
-                    <h1>En ce moment</h1>
-                    <h3>
-                        Retrouvez une sélection d'article d'actualité et de bonus inédits !
-                    </h3>
-                    <div class="figure-background row">
-                        <div class="col">
-                            <img src="../CasISEN/img/gamdom.jpg" alt="Logo" class="col-4-img">
-                            <a href="https://www.gamdom.com/" class="">
-                                <div>
-                                    <span class="url-title">Gamdom</span>
-                                </div>
-                                <span class="url-desc">gandom.com</span>
-                            </a>
-                        </div>
-                        <div class="col-md-8">
-                            <p>Un coffre <strong>GRATUIT</strong><br>et jusqu'à 50$ avec le code <strong>CASISEN</strong></p>
-                        </div>
-                        <div class="col">
-                            <svg class="svg-green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 89 70" id="gamdomarrow"><g fill="none" fill-rule="evenodd"><g mask="url(#ayb)"><path fill="#0EE07B" d="M26.253 37L26 44l-62 63h28.458L54 44v-7z"></path><path fill="#0EE07B" d="M26 40h27.646v-4.315l.354-.36V17.349c0-3.595-1.098-6.636-3.225-9.153L26 33.169V40z"></path><path fill="#0EE07B" d="M131.305-40.452L141-30.516V-66h-35.547l10.054 9.935-64.732 64.26-16.851 17.021L54.107 33"></path><path fill="#0A7843" d="M26 44h28V33H26z"></path></g></g></svg>
-                        </div>
-                    </div>
-                </div>-->
-
-
-    </section>
 
     </main>
 
