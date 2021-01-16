d5e32b6456defa8172bb617d62599896d09ab5a2
zZeTraL <lucashbs.pro@gmail.com>
2021-01-16 02:55:30 +0100

    This update brings some new contents

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
         <link rel="stylesheet" href="../CasISEN/css/header.css">
         <link rel="stylesheet" href="../CasISEN/css/hover.css">
         <link rel="stylesheet" href="../CasISEN/css/main.css">
+        <link rel="stylesheet" href="../CasISEN/css/footer.css">
 
         <!-- FONTAWESOME LIB -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -31,7 +32,7 @@
     </header>
 
     <main class="aos-animate">
-        <article class="h-500">
+        <article class="h-275">
             <div class="container-section aos-animate" data-aos="fade">
                 <h1>Bienvenue sur Cas'ISEN !</h1>
                 <h3>
@@ -41,7 +42,7 @@
             </div>
             <section class="section ptsforts-background">
                 <div class="container-section aos-animate" data-aos="zoom-in">
-                    <h1>Nos points forts</h1>
+                    <h2>Nos points forts</h2>
                     <div class="ptsforts-col">
                         <div class="ptsforts-col-element aos-animate" data-aos="zoom-in">
                             <img src="../CasISEN/img/ponctualite.svg" alt="Logo" class="col-img">
@@ -63,7 +64,7 @@
             </section>
             <section class="section">
                 <div class="container-section aos-animate" data-aos="fade">
-                    <h1>En ce moment</h1>
+                    <h2>En ce moment</h2>
                     <h3>
                         Retrouvez une sélection d'article d'actualité et de bonus inédits !
                     </h3>
@@ -78,14 +79,14 @@
                                     <span class="url-desc">gandom.com</span>
                                 </a>
                             </div>
-                            <svg class="svg-green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 89 70" id="gamdomarrow"><g fill="none" fill-rule="evenodd"><g mask="url(#ayb)"><path fill="#0EE07B" d="M26.253 37L26 44l-62 63h28.458L54 44v-7z"></path><path fill="#0EE07B" d="M26 40h27.646v-4.315l.354-.36V17.349c0-3.595-1.098-6.636-3.225-9.153L26 33.169V40z"></path><path fill="#0EE07B" d="M131.305-40.452L141-30.516V-66h-35.547l10.054 9.935-64.732 64.26-16.851 17.021L54.107 33"></path><path fill="#0A7843" d="M26 44h28V33H26z"></path></g></g></svg>
+                            <svg class="svg-green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 89 70"><g fill="none" fill-rule="evenodd"><g mask="url(#ayb)"><path fill="#0EE07B" d="M26.253 37L26 44l-62 63h28.458L54 44v-7z"></path><path fill="#0EE07B" d="M26 40h27.646v-4.315l.354-.36V17.349c0-3.595-1.098-6.636-3.225-9.153L26 33.169V40z"></path><path fill="#0EE07B" d="M131.305-40.452L141-30.516V-66h-35.547l10.054 9.935-64.732 64.26-16.851 17.021L54.107 33"></path><path fill="#0A7843" d="M26 44h28V33H26z"></path></g></g></svg>
                             <div class="figure-desc">
                                 <p>Un coffre <strong>GRATUIT</strong><br>et jusqu'à 50$ avec le code 
                                 <strong>"CASISEN"</strong></p>
                             </div>
                             <div class="button-resp">
                                 <button type="button" class="button-bonus">
-                                    <a href="">
+                                    <a href="#test">
                                         Récupère <b>ton bonus</b>
                                     </a>
                                 </button>
@@ -103,19 +104,42 @@
                                     <span class="url-desc">gandom.com</span>
                                 </a>
                             </div>
-                            <svg class="svg-green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 89 70" id="gamdomarrow"><g fill="none" fill-rule="evenodd"><g mask="url(#ayb)"><path fill="#0EE07B" d="M26.253 37L26 44l-62 63h28.458L54 44v-7z"></path><path fill="#0EE07B" d="M26 40h27.646v-4.315l.354-.36V17.349c0-3.595-1.098-6.636-3.225-9.153L26 33.169V40z"></path><path fill="#0EE07B" d="M131.305-40.452L141-30.516V-66h-35.547l10.054 9.935-64.732 64.26-16.851 17.021L54.107 33"></path><path fill="#0A7843" d="M26 44h28V33H26z"></path></g></g></svg>
+                            <svg class="svg-green" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 89 70"><g fill="none" fill-rule="evenodd"><g mask="url(#ayb)"><path fill="#0EE07B" d="M26.253 37L26 44l-62 63h28.458L54 44v-7z"></path><path fill="#0EE07B" d="M26 40h27.646v-4.315l.354-.36V17.349c0-3.595-1.098-6.636-3.225-9.153L26 33.169V40z"></path><path fill="#0EE07B" d="M131.305-40.452L141-30.516V-66h-35.547l10.054 9.935-64.732 64.26-16.851 17.021L54.107 33"></path><path fill="#0A7843" d="M26 44h28V33H26z"></path></g></g></svg>
                             <div class="figure-desc">
                                 <p>Un coffre <strong>GRATUIT</strong><br>et jusqu'à 50$ avec le code 
                                 <strong>"CASISEN"</strong></p>
                             </div>
                             <div class="button-resp">
                                 <button type="button" class="button-bonus">
-                                    <a href="">
+                                    <a href="#test">
                                         Récupère <b>ton bonus</b>
                                     </a>
                                 </button>
                             </div>
                         </div>
+                    </div>
+                    <div class="article-grid aos-animate" data-aos="flip-right" data-aos-delay="650" data-aos-duration="1000">
+                        <a href="../CasISEN/articles/article1.php">
+                            <div class="article-background article-flex-items hvr-shrink">
+                                <div class="article-grid-gradient"></div>
+                                <img class="article-grid-img" src="../CasISEN/img/article1.jpg" alt="article-image" >
+                                <span>Le casino en ligne, des débuts à nos jours</span>
+                            </div>
+                        </a>
+                        <a href="#test">
+                            <div class="article-background article-flex-items hvr-shrink">
+                                <div class="article-grid-gradient"></div>
+                                <img class="article-grid-img" src="../CasISEN/img/article2.jpg" alt="article-image" >
+                                <span>Roshtein gagne 325 000 € sur Fruit Party !!</span>
+                            </div>
+                        </a>
+                        <a href="#test">
+                            <div class="article-background article-flex-items hvr-shrink">
+                                <div class="article-grid-gradient"></div>
+                                <img class="article-grid-img" src="../CasISEN/img/article3.jpg" alt="article-image" >
+                                <span>Comment bien gérer sa bankroll</span>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </section>
@@ -124,9 +148,7 @@
         </article>
     </main>
 
-    <footer class="aos-animate">
-
-    </footer>
+    <?php require "../CasISEN/parts/footer.php"; ?>
 
     <!--<script src="../CasISEN/js/scroll.js"></script>-->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
--- parts/nav.php
+++ parts/nav.php
@@ -1,19 +1,11 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-
-<body>
-        <nav class="nav-container">
-            <a class="flip-animation" href="index.php"><img src="../CasISEN/img/logo.png" alt="Logo" class="nav-logo"></a>
-            <button class="become-vip-button hvr-pulse-grow">
-                <a href=""><i class="fa fa-diamond" aria-hidden="true"></i>Devenir VIP</a>
-            </button>
-            <a class="hvr-grow" href="actualites.php"><i class="fa fa-newspaper-o" aria-hidden="true"></i>Actualités</a>
-            <a class="hvr-grow" href="casino.php"><i class="fa fa-money" aria-hidden="true"></i>Casino</a>
-            <a class="hvr-grow" href="popular-games.php"><i class="fa fa-fire" aria-hidden="true"></i>Jeux populaires</a>
-        </nav>
-</body>
-</html>
+<nav class="nav-container">
+    <a class="flip-animation" href="index.php">
+        <img src="../CasISEN/img/logo.png" alt="Logo" class="nav-logo">
+    </a>
+    <button class="become-vip-button hvr-pulse-grow">
+        <a href=""><i class="fa fa-diamond" aria-hidden="true"></i>Devenir VIP</a>
+    </button>
+    <a class="hvr-grow" href="actualites.php"><i class="fa fa-newspaper-o" aria-hidden="true"></i>Actualités</a>
+    <a class="hvr-grow" href="casino.php"><i class="fa fa-money" aria-hidden="true"></i>Casino</a>
+    <a class="hvr-grow" href="popular-games.php"><i class="fa fa-fire" aria-hidden="true"></i>Jeux populaires</a>
+</nav>
